0f6a82ebec2e9dd748342792863390eb2441f832
hanneskuhltrakken <hannes@trakken.de>
2022-12-01 22:26:00 +0100

    code hightlightig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
-  content: {},
+  content: {
+    highlight: {
+      theme: "github-dark",
+    },
+  },
+
+
   // ssr: false,
   // nitro: {
   //   preset: "service-worker",
